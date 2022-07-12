806dcda23a9f71592bd57ff1a23403eae35eb039
Kemuel Nalifuma <kemnygh@gmail.com>
2022-07-12 14:34:09 +0300

    Create engineer and site form templates

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,5 +1,5 @@
 -- CREATE DATABASE site_maintenance;
--- CREATE TABLE engineers (id serial PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, eng_no VARCHAR, phone_no VARCHAR, email VARCHAR, created TIMESTAMP, updated TIMESTAMP, deleted VARCHAR DEFAULT 'FALSE');
--- CREATE TABLE sites (id serial PRIMARY KEY, name VARCHAR, description varchar, engineer_id INTEGER, created TIMESTAMP, updated TIMESTAMP, deleted VARCHAR DEFAULT 'FALSE');
+-- CREATE TABLE engineers (id serial PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, eng_no VARCHAR UNIQUE, phone_no VARCHAR UNIQUE, email VARCHAR UNIQUE, created TIMESTAMP, updated TIMESTAMP, deleted VARCHAR DEFAULT 'FALSE');
+-- CREATE TABLE sites (id serial PRIMARY KEY, name VARCHAR UNIQUE, description varchar, engineer_id INTEGER, created TIMESTAMP, updated TIMESTAMP, deleted VARCHAR DEFAULT 'FALSE');
 -- CREATE DATABASE site_maintenance_test WITH TEMPLATE site_maintenance;
--- select * from engineers;
+select * from engineers;
